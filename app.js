ddf065eef99e417ac39777486e64bab436fe8f10
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2023-07-10 12:30:21 +0530

    latest changes

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { isArray } = require("util");
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(cors({
+//     origin: ['http://rays.snmih.in']
+// }));
+
+app.use(cors({
+    origin: '*'
+}));
+
+
+const alupacRoute = require('./routes/alupac');
+
+app.use(alupacRoute);
+
+app.listen(process.env.PORT || 3333, () => {
+    console.clear();
+    console.log("Application listening on port 3333!");
+});
+
+
---
+++ controller/alupac.js
@@ -0,0 +1,46 @@
+const { response } = require('express');
+const nodemailer = require('nodemailer');
+let _STATUSCODE = 200;
+
+
+exports.addContactForm = (req, res, next) => {
+    let params = Object.assign(req.params, req.body);
+    addContactForm(params).then((result) => {
+        res.status(_STATUSCODE).json(result);
+    });
+};
+
+function addContactForm(objParam) {
+    console.log(objParam);
+
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'websupport@alupac.com',
+            pass: 'Alu@12345' // Replace with your email password
+        }
+    });
+
+    const mailOptions = {
+        from: 'ajay@spakcomm.com', // Replace with your email address
+        to: 'shiv@spakcomm.com, enquiry@alupac.com', // Replace with the recipient's email address
+        subject: 'Alupac Enquiry',
+        html: `
+                <b>Name:</b> ${objParam.uname}, <br />
+                <b>Email:</b> ${objParam.uemail}, <br />
+                <b>Location:</b> ${objParam.Location}, <br />
+                <b>Company:</b> ${objParam.companyname}, <br />
+                <b>Commment:</b> ${objParam.comment}, <br />
+                 `
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
+};
